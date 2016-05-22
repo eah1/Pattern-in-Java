2f9f2ca1c23b2534765184d65a3c50ea5ba5ee85
eah1 <eduardoarnedo90@gmail.com>
2016-05-22 19:16:15 +0200

    Initial commit with contributors

---
+++ togit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ..
+rm -rf Pattern-in-Java/*~
+cd Pattern-in-Java
+echo 'Step 1: git pull'
+echo 'Step 2: git add *'
+echo 'Step 3: git commit -m "<message>"'
+echo 'Step 4: git push -u origin master'
